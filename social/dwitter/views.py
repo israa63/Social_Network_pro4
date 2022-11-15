97b96e17e10d610777e215affedfdde38fb35f1e
israa <israa63>
2022-11-15 20:42:54 +0200

    Demoo

---
+++ social/dwitter/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Profile
+def dashboard(request):
+    return render(request, "base.html")
+
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, "dwitter/profile_list.html", {"profiles": profiles})  
+
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    return render(request, "dwitter/profile.html", {"profile": profile})      
